234fdb8d62d339719e0e6a8f038e38436e6e6b46
majdifox <majmehdi20@gmail.com>
2026-02-21 08:26:29 +0000

    creating the job model

---
+++ src/app/core/models/job.model.ts
@@ -0,0 +1,11 @@
+export interface Job {
+  id: string;
+  title: string;
+  company: string;
+  location: string;
+  description: string;
+  url: string;
+  salary: string;
+  datePosted: string;
+  apiSource: string;
+}
