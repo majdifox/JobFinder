4d6d0f7fdb8786d2470e4c2a35875ba090cb6f66
majdifox <majmehdi20@gmail.com>
2026-02-21 08:27:37 +0000

    creating the fav model

---
+++ src/app/core/models/favorite.model.ts
@@ -0,0 +1,11 @@
+export interface Favorite {
+    id?: number;
+    userId: number;
+    offerId: string;
+    title: string;
+    company: string;
+    location: string;
+    url: string;
+    apiSource: string;
+    dateAdded: string;
+}
